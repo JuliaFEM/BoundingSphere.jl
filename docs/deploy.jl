70e3167bba4305e1562fe3007abf9a1ab2977530
Jukka Aho <ahojukka5@gmail.com>
2018-07-24 09:59:43 +0300

    Setup Documenter
    Closes issue #10.

---
+++ docs/deploy.jl
@@ -0,0 +1,10 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/BoundingSphere.jl/blob/master/LICENSE
+
+using Documenter
+
+deploydocs(
+    repo = "github.com/JuliaFEM/BoundingSphere.jl.git",
+    julia = "0.6",
+    deps = nothing,
+    make = nothing)
---
+++ docs/src/index.md
@@ -0,0 +1,10 @@
+# Introduction
+
+Package contains algorithms to calculate smallest enclosing sphere for a given
+set of points in N dimensions.
+
+```@meta
+DocTestSetup = quote
+    using BoundingSphere
+end
+```
